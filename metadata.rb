b36545112d4a78f7b9a5da7b68cf25844d71b3dc
Scott M. Likens <scott@likens.us>
2013-05-10 22:49:04 +0000

    bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'scott@likens.us'
 license          'Apache 2.0'
 description      'Installs/Configures einarc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 recipe "einarc::install", "Installs einarc"
-supports 'ubuntu', "= 12.04"
+supports 'ubuntu'
 depends "build-essential"
 depends "git"
